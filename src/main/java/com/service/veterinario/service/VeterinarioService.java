5511f1eb1b32e9c096142ae3d7bf515d293d731e
Jaime <jai.hernandeza@duocuc.cl>
2025-06-05 16:37:46 -0400

    Faltó anotaciones

--- src/main/java/com/service/veterinario/service/VeterinarioService.java
+++ src/main/java/com/service/veterinario/service/VeterinarioService.java
@@ -3,10 +3,15 @@ package com.service.veterinario.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.service.veterinario.model.Veterinario;
 import com.service.veterinario.repository.VeterinarioRepository;
 
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class VeterinarioService {
     @Autowired
     private VeterinarioRepository veterinarioRepository;
